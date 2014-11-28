de6e1a463cacf467af607f15fec014c3a54b6d9d
Oliver Bestwalter <oliver@bestwalter.de>
2014-11-28 10:19:06 +0100

    test failure handling

--- svb-check.py
+++ svb-check.py
@@ -10,8 +10,9 @@ def test_svb(capfd):
     scriptPath = os.path.join(here, 'svb-check-cmd.sh')
     process = subprocess.Popen(scriptPath)
     process.communicate()
-    assert process.returncode == 0, process.returncode
     out, err = capfd.readouterr()
+    if process.returncode:
+        pytest.fail("%s (exitcode %s)" % (err, process.returncode))
     assert "2.7.8" in out and '3.4.2' in out
 
 if __name__ == '__main__':
